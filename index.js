be8376b28f321036c8e4a46b6b1299b61ced6754
JakeHowdeshell <135374279+JakeHowdeshell@users.noreply.github.com>
2023-08-22 09:31:25 -0500

    updated the exported code to utilize the class's created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
+const path = require("path");
 const fs = require("fs");
-const {createLogo} = require('./lib/shapes.js')
+const { Shapes, Circle, Triangle, Square } = require("./lib/shapes.js");
 
 inquirer
   .prompt([
@@ -24,7 +25,9 @@ inquirer
         const colorRegex = /^#?([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
         if (
           input.match(colorRegex) ||
-          /^(red|blue|green|yellow|purple|pink|orange|brown)$/i.test(input)
+          /^(red|blue|green|yellow|purple|pink|orange|brown|white|black)$/i.test(
+            input
+          )
         ) {
           return true;
         }
@@ -39,7 +42,7 @@ inquirer
     {
       type: "list",
       message: "What shape would you like your logo?",
-      choices: ["Circle", "Triangle", "Square"],
+      choices: ["circle", "triangle", "square"],
       name: "shape",
     },
     {
@@ -50,7 +53,9 @@ inquirer
         const colorRegex = /^#?([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
         if (
           input.match(colorRegex) ||
-          /^(red|blue|green|yellow|purple|pink|orange|brown)$/i.test(input)
+          /^(red|blue|green|yellow|purple|pink|orange|brown|white|black)$/i.test(
+            input
+          )
         ) {
           return true;
         }
@@ -60,16 +65,30 @@ inquirer
     },
   ])
   .then(function (data) {
-    //logo = createLogo(data);
+    let newShape;
+    switch (data.shape) {
+      case "circle":
+        newShape = new Circle(data).createLogo();
+        break;
+      case "triangle":
+        newShape = new Triangle(data).createLogo();
+        break;
+      case "square":
+        newShape = new Square(data).createLogo();
+        break;
+      default:
+        console.error("Invalid shape choice");
+        return;
+    }
     const outputFolder = path.join(__dirname, "examples", "logo.svg");
-    fs.writeFile(outputFolder, logo, (err) =>
+    fs.writeFile(outputFolder, newShape, (err) =>
       err ? console.error(err) : console.log("Generated logo.svg")
     );
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
   });
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
